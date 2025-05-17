82b8213dcd5c2563b7655fa292d29efc759c1efb
Matheus Santana <302math302@gmail.com>
2025-05-16 23:30:23 -0300

    primeiro commit

---
+++ aluguel-jogos/src/main/java/projeto/aluguel_jogos/AluguelJogosApplication.java
@@ -0,0 +1,13 @@
+package projeto.aluguel_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelJogosApplication.class, args);
+	}
+
+}
---
+++ aluguel-jogos/src/test/java/projeto/aluguel_jogos/AluguelJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.aluguel_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
